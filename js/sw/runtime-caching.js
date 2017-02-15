4cd18cf543b4e16dd931705f61d79c8411794325
Vitaliy Bobrov <bobrov1989@gmail.com>
2017-02-15 20:10:58 +0100

    Updates blog content Wed, 15 Feb 2017 19:10:56 GMT

--- js/sw/runtime-caching.js
+++ js/sw/runtime-caching.js
@@ -1,7 +1,1 @@
-(global => {
-  'use strict';
-
-  global.toolbox.router.get('/(.*)', global.toolbox.fastest, {
-    origin: /\.(?:googleapis|gstatic|google-analytics|bobrov-blog.disqus)\.com$/
-  });
-})(self);
+"use strict";!function(o){o.toolbox.router.get("/(.*)",o.toolbox.fastest,{origin:/\.(?:googleapis|gstatic|bobrov-blog.disqus)\.com$/})}(self);
